27f7ab8c0d1ee206cd3697e512f2d0f6db7eb694
ratinac-nikola <ratinac.nikola@gmail.com>
2022-05-03 13:46:03 +0200

    feat(training): minibatch training

--- train.py
+++ train.py
@@ -13,32 +13,41 @@ else:
     device = "cpu"
 
 
-dataset = Dataset(device=device)
+dataset = Dataset(device=device, name="ml-100k")
 tensorData = dataset.getDatasetAsTensor()
-trainset, testset = train_test_split(tensorData, train_size=0.75, random_state=42)
-print("Dataset loaded")
+trainset, testset = train_test_split(tensorData, train_size=0.9, random_state=42)
+print(trainset.shape, testset.shape)
 
 
-rbm = RBM(trainset.shape[1], n_hidden=10, device=device, learning_rate=0.1)
+rbm = RBM(
+    trainset.shape[1],
+    n_hidden=20,
+    device=device,
+    learning_rate=0.1,
+    batch_size=config.batch_size,
+    momentum=True,
+    alpha=0.9,
+)
 
 for epoch in range(1, config.epochs + 1):
+    print(f"Epoch\t{epoch}", end="\t")
+    se_val = 0
+    ae_val = 0
+    ratings_count = 0
+    trainset = trainset[torch.randperm(len(trainset))]
+    for user in range(0, len(trainset), config.batch_size):
 
-    for vin in trainset:
+        minibatch = trainset[user : user + config.batch_size]
+        rbm.apply_gradient(minibatch=minibatch)
 
-        v0 = vin
-        vk = vin
-
-        for k in range(10):
-            _, hk = rbm.sample_h(vk)  # forward pass
-            _, vk = rbm.sample_v(hk)  # backward pass
-            vk[v0.sum(dim=1) == 0] = v0[v0.sum(dim=1) == 0]
-
-        rbm.train(v0, vk)
-
-    print(f"Epoch {epoch} done.", end=" ")
-    rmse = 0
+    se = 0
+    ae = 0
+    ratings_count = 0
     for vin in testset:
         rec = rbm.reconstruct(vin)
-        # print(reconstruction_rmse(vin, rec))
-        rmse += reconstruction_rmse(vin, rec)
-    print(rmse / len(testset))
+        ratings_count += len(vin[vin.sum(dim=1) > 0])
+        se += squared_error(vin, rec)
+        ae += absolute_error(vin, rec)
+
+    print("TestRMSE\t", sqrt(se / ratings_count), end="\t")
+    print("TestMAE\t", ae / ratings_count)
